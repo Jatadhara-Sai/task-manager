b98617cffc1c7d06064586a59eb0797afc909323
KATTA JATADHARA SAI <118649537+Jatadhara-Sai@users.noreply.github.com>
2025-05-27 11:27:34 +0530

    Update apollo-client.js

--- task-management-frontend/src/lib/apollo-client.js
+++ task-management-frontend/src/lib/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://task-manager-1-x390.onrender.com',
+  uri: 'https://task-manager-1-x390.onrender.com/graphql', 
   cache: new InMemoryCache(),
 });
 
