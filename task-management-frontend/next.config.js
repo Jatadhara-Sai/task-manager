a1925305bd37abedd9a9c4811aea8ef9439ca5cd
KATTA JATADHARA SAI <118649537+Jatadhara-Sai@users.noreply.github.com>
2025-05-27 11:40:09 +0530

    Create next.config.js

---
+++ task-management-frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build', // Optional: changes output folder to 'build'
+};
+
+module.exports = nextConfig;
